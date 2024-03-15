0d81dfccf60541bd93a98d11fc1e597c022e0bc9
MiniPandi <n.j.e.leist@gmail.com>
2024-03-15 18:02:37 +0100

    a

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+setInterval(updateStatus,1000)
+
+function updateStatus() {
 fetch('https://hosting-miniclient.wireway.ch/status')
-.then(response => response.json())
-.then(data => {
-    document.getElementById('onlinePlayers').innerText = data.onlinePlayers + ' Online';
-});
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('onlinePlayers').innerText = data.onlinePlayers + ' Online';
+    });
+}
